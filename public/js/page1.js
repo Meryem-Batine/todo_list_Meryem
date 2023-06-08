97b9971a66e60599864b85447da63b8f7a96657f
Meryem-Batine <meryemnour200@gmail.com>
2023-06-08 17:17:00 +0100

    first

---
+++ public/js/page1.js
@@ -0,0 +1,65 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let added=document.querySelector(".added");
+// let content=document.querySelector(".content");
+// Verifie si input remplis
+    // for (let index = 0; index < add_btn.length; index++) {
+    //     let element = add_btn[index];
+    //     const add = () =>{
+    //         if (text_input.length == "" || text_input.length == 0) {
+    //             alert("Remplis ton text");
+    //         }else if (text_input.length != "" || text_input.length != 0) {
+    //             content.textContent+=text_input.value;
+    //         }
+    //     }
+    //     element.addEventListener("click", add)
+    //     console.log(hbug);
+    // }
