c7cf0d30986eb33212a6477b72a19eff4439c1c1
Meryem-Batine <meryemnour200@gmail.com>
2023-06-09 08:20:28 +0100

    third

--- public/js/page1.js
+++ public/js/page1.js
@@ -7,60 +7,44 @@ let all_select = document.querySelector("#all-select");
 let select_1 = document.querySelector("#select1");
 let select_2 = document.querySelector("#select2");
 let all_text = [];
-console.log(div_parent);
+let all_element =[];
+let vrf_element =[];
 
 // * creer paraghraphe
 for (let index = 0; index < add_btn.length; index++) {
     let element = add_btn[index];
     const add = () => {
         let vlu =text_input.value;
+        all_element.push(vlu);
         text_input.value = "";
         let div = document.createElement("div")
-        div.innerHTML = `        <div class="test">
+        div.innerHTML = `<div class="test">
     <p class="p-text">${vlu}</p>
     <i class="fa-solid fa-pen fa-lg modif" style="color: #005cfa;"></i>
     <i class="fa-solid fa-check fa-xl valide" style="color: #09ae0c;"></i>
     <i class="fa-solid fa-xmark fa-xl supprm" style="color: #f01000;"></i>
     </div>`
         box.appendChild(div)
-        box.appendChild(i);
-        box.appendChild(supp);
-        box.appendChild(mod);
-        i.addEventListener("click", () => {
-            div.textContent.style.color = "green"
-        })
-        supp.addEventListener("click", () => {
-            div.remove();
-            i.remove();
-            supp.remove();
-            mod.remove();
-        })
-        mod.addEventListener("click", () => {
-            let ask = confirm(`do you want to change this text`);
-            if (ask == true) {
-                let change = prompt(`change your text`);
-                div.textContent = change;
-            }
-        })
-        if (text_input.value = "") {
-            alert("no");
-        }
     }
     element.addEventListener("click", add);
 }
+//* function icons
 box.addEventListener("click",(element)=>{
+    //! pour valider
     if(element.target.classList.contains("valide"))
     {
     let parent =element.target.parentElement;
-    parent.style.color="green"
+    parent.style.color="green";
+    vrf_element.push(parent)
     }
-    
+        //! pour supprimer
     if(element.target.classList.contains("supprm"))
     {
     let parent =element.target.parentElement;
     parent.remove();
+    all_element.push(parent);
     }
-
+    //! pour modifier
     if(element.target.classList.contains("modif"))
     {
     let para =element.target.previousElementSibling;
@@ -68,10 +52,28 @@ box.addEventListener("click",(element)=>{
             if (ask == true) {
                 let change =prompt(`change your text`);
                 para.textContent= change;
+                all_element.push(parent);
+                console.log(all_element);
             }
     }
-    
-    });
+});
+console.log(vrf_element);
+console.log(all_element);
+
+//* select
+function addselect() {
+    let slct=document.querySelector("#select");
+    let slct_value=slct.value;
+    let selectElement=[];
+    if (slct ==="1") {
+        for (let index = 0; index < all_element.length; index++) {
+            let element = all_element[index];
+            selectElement.push(element);
+        }
+    }
+}
+
+
 
 
 
---
+++ public/js/page2.js
@@ -0,0 +1,51 @@
+let input_2 = document.querySelector("#input-text-p2");
+let btn_2 = document.querySelectorAll("#btn-ajouter-p2");
+let todo_box = document.querySelector(".todo");
+let all_text_input = [];
+console.log(btn_2);
+// * creer paraghraphe
+for (let index = 0; index < btn_2.length; index++) {
+    let element_2 = btn_2[index];
+    const addin2 = () => {
+        let value_2 = input_2.value;
+        all_text_input.push(value_2);
+        input_2.value = "";
+        let div = document.createElement("div")
+        div.innerHTML = `<div class="test_2">
+    <p class="p-text-2">${value_2}</p>
+    <i class="fa-solid fa-pen fa-lg modif-2" style="color: #005cfa;"></i>
+    <i class="fa-solid fa-xmark fa-xl supprm-2" style="color: #f01000;"></i>
+    <button type="button" class="btn btn-success dropdown-toggle dropdown-toggle-split drop-btn" data-bs-toggle="dropdown" aria-expanded="false">
+    <span class="visually-hidden">Toggle Dropend</span>
+    </button>
+    <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">2</a></li>
+        <li><a class="dropdown-item" href="#">3</a></li>
+    </div>`
+        todo_box.appendChild(div)
+    }
+    element_2.addEventListener("click", addin2);
+}
+
+//* function icons et select
+todo_box.addEventListener("click",(element)=>{
+        //! pour supprimer
+    if(element.target.classList.contains("supprm"))
+    {
+    let content =element.target.parentElement;
+    content.remove();
+    all_text_input.push(content);
+    }
+    //! pour modifier
+    if(element.target.classList.contains("modif"))
+    {
+    let para_p2 =element.target.previousElementSibling;
+            let ask =confirm(`do you want to change this text`);
+            if (ask == true) {
+                let change =prompt(`change your text`);
+                para_p2.textContent= change;
+                all_text_input.push(content);
+                console.log(all_text_input);
+            }
+    }
+});
